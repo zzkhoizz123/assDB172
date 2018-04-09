5bcbb31213a4c10f541897b609423fe30043eef4
khoitran <zzkhoizz123@gmail.com>
2018-04-09 21:36:11 +0700

    second commit

--- NodejsWebApp5/server.js
+++ NodejsWebApp5/server.js
@@ -8,17 +8,11 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/views'));
 
-//app.get('/', function (req, res) {
-//    res.sendFile(__dirname + '/index.html');
-//});
 
 app.get('/', function (req, res) {
-    res.render('Home'); // it will automatically read in file views : index.ejs
+    res.render('Home/Home'); // it will automatically read in file views : index.ejs
 });
 
-app.get('/contact', function (req, res) {
-    res.render('contact'); // it will automatically read in file views : contact.ejs
-});
 
 app.post('/', function (req, res) {
     // console.log(req.body['userEmail']);
@@ -28,13 +22,6 @@ app.post('/', function (req, res) {
     res.render('Home');
 
 });
-
-
-app.get('/profile/:name', function (req, res) {
-
-    var data = { age: 29, job: 'student', hobbies: ['eating', 'listening', 'fishing'] };
-    res.render('profile', { person: req.params.name, data: data }); // this is the name of the file in view
-});                                                     // then u can tranfer the object as data to file .ejs
 
 app.all('/*', (req, res) => {
     res.send('Not found');
